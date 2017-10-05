a14dcf0119a02ac4d04c081ad74b82cb8b42b008
Eric Hemphill <hemphill.39@osu.edu>
2017-10-05 18:55:32 -0400

    removed username and password from source code. Added them as env variables

--- welcome.py
+++ welcome.py
@@ -13,59 +13,51 @@
 # limitations under the License.
 
 import os
+import json
 from flask import Flask, jsonify, render_template, request, redirect, url_for
-#from forms import QueryForm
-import discovery
-
+from discovery import Discovery
 
 app = Flask(__name__)
+
+discovery = None
+
+discovery_collection_id="c31902df-8069-4ea2-9c75-746336721525"
+discovery_configuration_id="2d31d73a-5679-49a6-9730-63d3519b6a74"
+discovery_environment_id="0cfdc99b-3b1e-4b0b-b5ec-bfebf5d250dd"
+
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.getenv('VCAP_SERVICES'))
+    if 'discovery' in vcap:
+        print 'Found VCAP_SERVICES'
+        creds = vcap['discovery'][0]['credentials']
+        user = vcap['username']
+        password = creds['password']
+        url = creds['url']
+        discovery = Discovery(url, user, password, discovery_collection_id, discovery_configuration_id,
+                              discovery_environment_id)
+elif os.path.isfile('vcap-local.json'):
+    with open('vcap-local.json') as f:
+        vcap = json.load(f)
+        print 'Found local VCAP_SERVICES'
+        creds = vcap['discovery'][0]['credentials']
+        user = vcap['username']
+        password = creds['password']
+        url = creds['url']
+        discovery = Discovery(url, user, password, discovery_collection_id, discovery_configuration_id,
+                              discovery_environment_id)
+
 
 @app.route('/')
 def Welcome():
     return app.send_static_file('index.html')
-
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
-
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
-
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
 
 @app.route('/api/query/<query>')
 def query_watson(query):
     return jsonify(result=handle_input(query))
 
 def handle_input(user_input):
+    discovery = Discovery(app.config['discovery_url'], app.config['discovery_username'], app.config['discovery_password'], app.config['discovery_collection_id'], app.config['discovery_configuration_id'], app.config['discovery_environment_id'])
     return discovery.query(user_input)
-
-
-"""
-@app.route('/query', methods=['GET', 'POST'])
-def query():
-    print "hey"
-    form = QueryForm()
-    print "sup"
-    if request.method == 'POST':
-        print "post"
-        return handle_input(request.form['query'])
-    elif request.method == 'GET':
-        print "get"
-        print type(render_template)
-        return render_template('query.html', form=form)
-"""
-
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
