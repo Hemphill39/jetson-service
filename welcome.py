0f29e575403325dc0e2142e1feee4b30298564a5
Eric Hemphill <hemphill.39@osu.edu>
2017-11-28 18:28:56 -0500

    fixed my stupid mistake

--- welcome.py
+++ welcome.py
@@ -94,7 +94,7 @@ def is_localhost(request_url):
 
 @app.before_request
 def before_request():
-    if request.url.startswith('http://') and not is_localhost(request_url):
+    if request.url.startswith('http://') and not is_localhost(request.url):
         url = request.url.replace('http://', 'https://', 1)
         code = 301
         return redirect(url, code=code)
